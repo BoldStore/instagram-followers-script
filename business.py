55835e21fb47606bf9fb32b35a76cd090deec4eb
avivashishta29 <avivashishta@gmail.com>
2022-12-19 15:04:13 +0530

    making sure account exists otherwise error

--- business.py
+++ business.py
@@ -1,11 +1,7 @@
-import requests
-import json
-import time
-
-
 import csv
-
 import instaloader
+import requests
+
 
 # Creating an instance of the Instaloader class
 bot = instaloader.Instaloader()
@@ -19,17 +15,11 @@ with open('followerCount.txt', 'w') as f:
         for row in csv_reader:
             line_count += 1
             username = row[0]
-            # username = username.replace(" ","" )
-            # print(username)
-            # data = requests.get('https://i.instagram.com/api/v1/users/web_profile_info/?username='+username, headers={"User-Agent":"Instagram 76.0.0.15.395 Android (24/7.0; 640dpi; 1440x2560; samsung; SM-G930F; herolte; samsungexynos8890; en_US; 138226743"})
-            # if data and data.status_code == 200 and json.loads(data.content)['data'] and json.loads(data.content)['data']['user']:
-            #     followers = json.loads(data.content)
-            #     f.write(str(followers['data']['user']['edge_followed_by']['count']))
-            #     f.write("\n")
-            # else: 
-            #     f.write("0")
-            #     f.write("\n")
-            # Loading a profile from an Instagram handle
-            profile = instaloader.Profile.from_username(bot.context, username)
-            print(line_count,"Followers Count: ", str(profile.followers))
-            f.write(str(profile.followers))
+            response = requests.get("https://instagram.com/" + username + "/")
+            if "https://www.instagram.com/"+username+"/" in response.text:
+                profile = instaloader.Profile.from_username(bot.context, username)
+                print(line_count,"Followers Count: ", str(profile.followers))
+                f.write(str(profile.followers))
+            else:
+                print(line_count,"Followers Count: ", "0")
+                f.write("0")
